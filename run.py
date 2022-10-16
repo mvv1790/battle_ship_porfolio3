9beb263dd6cfe9583b75ef0ef5357b12bd4a7a6d
mvv1790 <mvv1790@gmail.com>
2022-10-16 23:59:11 +0000

    Added functions for game board, battle ships, position and damage

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+"""
+
+"""
+# 8 x 8 board, game standard
+BATTLE_BOARD = [[' '] * 8 for x in range(8)]
+FOE_BOARD = [[' '] * 8 for x in range(8)]
+
+#letters to numbers conversion
+letters_numbers = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7}
+
+def game_board(game):
+    """
+    Defines board of the game
+    """
+    print(' a b c d e f g h')
+    print(' ---------------')
+    row_number = 1 
+    for row in game:
+        game_board[player_board].append('.')
+
+
+def create_battleship():
+    pass
+
+def seek_battleship():
+    pass
+
+def damage_done():
+    pass
