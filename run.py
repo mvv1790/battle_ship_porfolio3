c1fae333305743ff71cf02713cf09b019eb1fbbb
mvv1790 <mvv1790@gmail.com>
2022-10-17 03:46:14 +0000

    Adjusted game board styling, cleaned up the code, added start and win game messages

--- run.py
+++ run.py
@@ -4,29 +4,34 @@
 # Game's rules:
 # X for boat position
 # ' ' vacant space
-# - miss
+# O miss
 
 from random import randint
 """
 
 """
-# 8 x 8 board, game standard
-BATTLE_BOARD = [[' '] * 6 for x in range(6)]
-DAMAGE_BOARD = [[' '] * 6 for x in range(6)]
+# 6 x 6 game board
+BATTLE_BOARD = [[' '] * 7 for x in range(6)]
+DAMAGE_BOARD = [[' '] * 7 for x in range(6)]
 
 #letters to numbers conversion
 letters_numbers = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,}
 
-name = input('Enter your name:')
-print('Hello, ' + name) 
+# Player name input
+name = input("You shall be known as: ")
+print("Greetings, " + name)
+
+# Start game message
+print("The Battle Begins Admiral " + name)
 
 #create game board
 def game_board(game):
     """
     Defines the board of the game
+    Prints letters and the upper border of the game field
     """
-    print(' a b c d e f')
-    print(' -----------')
+    print('  a b c d e f')
+    print(' -------------')
     row_number = 1 
     for row in game:
         print("%d|%s" % (row_number, "|".join(row)))
@@ -52,14 +57,14 @@ def seek_battleship():
     """
     The loop of the game with messages depending on the outcome of an action
     """
-    row = input("Seek your foe's number...")
+    row = input("Seek your foe's number (from 1 to 6)...")
     while row not in '123456':
-        print("Try again!")
-        row = input("Launch your attack!")
-    column = input("Seek your foe's letter...")
+        print("Wrong input!")
+        row = input("Launch your attack! (numbers from 1 to 6)")
+    column = input("Seek your foe's letter (from 'a' to 'f')...")
     while column not in "abcdef":
-        print("Try again!")
-        column = input("Seek your foe's letter!")
+        print("Play by the rules!")
+        column = input("Seek your foe's letter! (letters from 'a' to 'f')")
     return int(row) - 1, letters_numbers[column]
 
 def damage_done(board):
@@ -72,15 +77,14 @@ def damage_done(board):
             if column == "X":
                 count += 1
     return count
-    
-create_battleship(BATTLE_BOARD)
 
+# The rule of 7 turns, after which the game ends
 turns = 7
 while turns > 0:
-    print("The battle begins!")
+
     game_board(DAMAGE_BOARD)
     row, column = seek_battleship()
-    if  DAMAGE_BOARD[row][column] == '-':
+    if  DAMAGE_BOARD[row][column] == 'O':
         print("This target has been hit before...")
     elif BATTLE_BOARD[row][column] == "X":
         print("Good shot!")
@@ -88,10 +92,10 @@ while turns > 0:
         turns -= 1
     else:
         print("Better get some goggles!")
-        DAMAGE_BOARD[row][column] = "-"
+        DAMAGE_BOARD[row][column] = "O"
         turns -= 1
     if damage_done(DAMAGE_BOARD) == 3:
-        print("You have won the battle, but not the war!")
+        print("You have won the battle" +name, "but not the war!")
         break
     if turns == 0:
         print("You have lost the battle " +name, "but not the war!")
