ec2f9e113a3f2096525fb2130ea001c38d583646
mvv1790 <mvv1790@gmail.com>
2022-10-17 01:03:22 +0000

    Decreased number of rows and columns to 6, fixed a bug where the game went out of number scope

--- run.py
+++ run.py
@@ -11,19 +11,19 @@ from random import randint
 
 """
 # 8 x 8 board, game standard
-BATTLE_BOARD = [[' '] * 8 for x in range(8)]
-FOE_BOARD = [[' '] * 8 for x in range(8)]
+BATTLE_BOARD = [[' '] * 6 for x in range(6)]
+FOE_BOARD = [[' '] * 6 for x in range(6)]
 
 #letters to numbers conversion
-letters_numbers = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7}
+letters_numbers = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,}
 
 #create game board
 def game_board(game):
     """
     Defines the board of the game
     """
-    print(' a b c d e f g h')
-    print(' ---------------')
+    print(' a b c d e f')
+    print(' -----------')
     row_number = 1 
     for row in game:
         print("%d|%s" % (row_number, "|".join(row)))
@@ -37,19 +37,19 @@ def create_battleship(board):
     Creates 3 battle ships on the board
     """
     for boat in range(3):
-        boat_row, boat_column = randint(0, 7), randint(0, 7)
+        boat_row, boat_column = randint(0, 5), randint(0, 5)
         while board[boat_row][boat_column] == 'X':
-            boat_row, boat_column = randint(0, 7), randint(0, 7) 
+            boat_row, boat_column = randint(0, 5), randint(0, 5) 
         board[boat_row][boat_column] = 'X'    
 
 
 def seek_battleship():
     row = input("Seek your foe's number!")
-    while row not in '12345678':
+    while row not in '123456':
         print("Try again!")
         row = input("Launch your attack!")
     column = input("Seek your foe's letter!")
-    while column not in "abcdfgh":
+    while column not in "abcdef":
         print("Try again!")
         column = input("Seek your foe's letter!")
     return int(row) - 1, letters_numbers[column]
@@ -68,3 +68,4 @@ create_battleship(BATTLE_BOARD)
 game_board(BATTLE_BOARD)
 game_board(FOE_BOARD)
 turns = 10
+#while turn > 0:
